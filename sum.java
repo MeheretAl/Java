e028151c554d81eebae54979181c0885c5311ab3
MeheretAl <meheretxo4@gmail.com>
2024-02-17 22:07:36 +0300

    added sum.java

---
+++ sum.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class sum {
+
+    public static void main(String[] Args) {
+        Scanner input = new Scanner(System.in);
+
+        int firstNum;
+        int secondNum;
+        int sum;
+
+        System.out.print("Enter First Num: ");
+        firstNum = input.nextInt();
+        System.out.print("Enter Second Num: ");
+        secondNum = input.nextInt();
+
+        sum = firstNum + secondNum;
+
+        System.out.printf("Sum is %d%n", sum);
+
+        input.close();
+
+    }
+
+}

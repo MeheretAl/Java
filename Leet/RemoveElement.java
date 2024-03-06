71e094f28c0c3e7c460831ad51b99bae4c19ebb6
MeheretAl <meheretxo4@gmail.com>
2024-03-06 18:42:04 +0300

    newer better implementation

--- Leet/RemoveElement.java
+++ Leet/RemoveElement.java
@@ -10,27 +10,11 @@ public class RemoveElement {
     }
 
     public static void removeElement(int[] nums, int target) {
-        int length = nums.length;
         int counter = 0;
-
-        for (int i = 0; i < length; i++) {
-            if (nums[i] == target) {
-                counter += 1;
-            }
-        }
-
-        int lastIndex = length - 1;
-        for (int i = 0; i < lastIndex; i++) {
-            if (nums[i] == target) {
-                while (lastIndex > i && nums[lastIndex] == target) {
-                    lastIndex--;
-                }
-                if (lastIndex > i) {
-                    int temp = nums[i];
-                    nums[i] = nums[lastIndex];
-                    nums[lastIndex] = temp;
-                    lastIndex--;
-                }
+        for (int num: nums){
+            if (num != target){
+                nums[counter] = num;
+                counter++;
             }
         }
     }

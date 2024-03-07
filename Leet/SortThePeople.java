2ef7c3a2279cba9814f5ad092c8223f7ebd222f4
MeheretAl <meheretxo4@gmail.com>
2024-03-08 00:14:53 +0300

    added sort the people,uses bubblesort

---
+++ Leet/SortThePeople.java
@@ -0,0 +1,31 @@
+package Leet;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class SortThePeople {
+    public static void main(String[] args) {
+        String[] names = {"Mary","John","Emma"};
+        int[] heights = {180,165,170};
+        String[] sorted = sortThePeople(names,heights);
+
+
+    }
+
+    public static String[] sortThePeople(String[] names, int[] heights){
+        int length = heights.length;
+        for (int i = 0; i < length-1;i++) {
+            for (int j = 0; j < length - 1 - i; j++) {
+                if (heights[j] < heights[j+1]){
+                    int swap  =  heights[j];
+                    String swapString = names[j];
+                    heights[j] = heights[j+1];
+                    names[j] = names[j+1];
+                    heights[j+1] = swap;
+                    names[j+1] = swapString;
+                }
+            }
+        }
+        return names;
+    }
+}
